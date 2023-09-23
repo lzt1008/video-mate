27523d001f0132e42acbb5d08f8863ea1675100a
lzt1008 <lzt1008@live.com>
2023-09-24 00:16:53 +0800

    feat(content): add query video listener

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,9 @@
 import { onMessage } from 'webext-bridge/content-script'
 import { OperationType } from '@/popup/types'
+import { queryVideo } from '@/sync'
 
-(() => {
-  console.info('[video-mate] Hello world from content script')
-  
-})()
+console.info('[video-mate] Hello world from content script')
+
+onMessage(OperationType.queryVideo, () => {
+  return queryVideo()
+})
