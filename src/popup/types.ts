6ed4114c0cc6ccce8940e9378ef33eb1103eaca6
lzt1008 <lzt1008@live.com>
2023-09-25 02:20:02 +0800

    feat: add type definition

--- src/popup/types.ts
+++ src/popup/types.ts
@@ -1,6 +1,32 @@
+import type { Socket } from 'socket.io-client'
+
 export enum OperationType {
   queryVideo = 'query-video',
+  queryRoom = 'query-room',
   joinRoom = 'join-room',
   crateRoom = 'crate-room',
   message = 'message',
+}
+
+export interface Message {
+  sender: {
+    id: string
+    username: string
+  }
+  socketId?: string
+  op: string
+  data: any
+  timestamp: number
+}
+
+export interface Room {
+  id: string
+  socketId: string
+  messages: Array<Message>
+}
+
+export interface Synchronizer {
+  room: Room
+  socket: Socket
+  videoEl: HTMLVideoElement
 }
